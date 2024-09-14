707433802fddafcfcf0dd398fc8801000d068ec4
SiddheshwarKanhere <siddheshwarkanhere79@gmail.com>
2024-09-14 22:48:18 +0530

    frist commit

---
+++ blog-app-api-1/src/main/java/in/kanhere/BlogAppApi1Application.java
@@ -0,0 +1,13 @@
+package in.kanhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApi1Application.class, args);
+	}
+
+}
---
+++ blog-app-api-1/src/test/java/in/kanhere/BlogAppApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kanhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
